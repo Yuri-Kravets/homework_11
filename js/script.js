dc6645ceedbe8b2067c93de948b88bc5d280e165
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2021-10-14 15:27:55 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let enterNum = +prompt('введите число');
+
+
+let result = true;
+for (let i = 2; i < enterNum; i++) {
+
+	if (enterNum % i == 0) {
+		result = false;
+		break; 
+	}
+}
+
+console.log(result);
