25843572b759d523d9c6374a4d32efc734b391ad
Yuri-Kravets <91874641+Yuri-Kravets@users.noreply.github.com>
2021-10-14 15:29:24 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let enterNum = +prompt('введите число');
 let result = true;
 for (let i = 2; i < enterNum; i++) {
 
-	if (enterNum % i == 0) {
+	if (enterNum % i === 0) {
 		result = false;
 		break; 
 	}
